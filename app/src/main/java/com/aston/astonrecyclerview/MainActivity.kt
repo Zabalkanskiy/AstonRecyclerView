0783f129f9e5afdd3b6f9563d97ae710a891fc0a
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-03 19:14:17 +0300

    Add icon trash

--- app/src/main/java/com/aston/astonrecyclerview/MainActivity.kt
+++ app/src/main/java/com/aston/astonrecyclerview/MainActivity.kt
@@ -2,9 +2,12 @@ package com.aston.astonrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aston.astonrecyclerview.databinding.ActivityMainBinding
 import com.aston.astonrecyclerview.model.RecyclerViewModel
+import com.aston.astonrecyclerview.recycler.MyRecyclerViewAdapter
 import com.aston.astonrecyclerview.repository.RecyclerViewRepository
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +21,26 @@ class MainActivity : AppCompatActivity() {
 
         val viewModelFactory = RecyclerViewModelFactory(RecyclerViewRepository())
         recyclerViewModel = ViewModelProvider(this, viewModelFactory).get(RecyclerViewModel::class.java)
+
+        val myRecyclerViewAdapter = MyRecyclerViewAdapter()
+        binding.mainActivityRecyclerView.adapter = myRecyclerViewAdapter
+
+        val repComtacts = recyclerViewModel.getContacts()
+
+        binding.mainActivityRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        myRecyclerViewAdapter.submitList(repComtacts)
+
+        binding.appCompatImageButton.setOnClickListener {
+            binding.mainActiviytyButtonAddContact.visibility = View.INVISIBLE
+            binding.mainActiviytyButtonDelete.visibility = View.VISIBLE
+            binding.mainActiviytyButtonCancel.visibility = View.VISIBLE
+        }
+
+        binding.mainActiviytyButtonCancel.setOnClickListener {
+            binding.mainActiviytyButtonAddContact.visibility = View.VISIBLE
+            binding.mainActiviytyButtonDelete.visibility = View.INVISIBLE
+            binding.mainActiviytyButtonCancel.visibility = View.INVISIBLE
+        }
     }
 }
