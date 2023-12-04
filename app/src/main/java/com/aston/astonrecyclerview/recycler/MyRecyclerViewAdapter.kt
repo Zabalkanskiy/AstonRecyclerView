252dcc795bb0ef0c1a206be7da183a490b01f751
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-04 19:40:38 +0300

    add checkBox field in onClickAction

--- app/src/main/java/com/aston/astonrecyclerview/recycler/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/aston/astonrecyclerview/recycler/MyRecyclerViewAdapter.kt
@@ -2,22 +2,26 @@ package com.aston.astonrecyclerview.recycler
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.aston.astonrecyclerview.R
 import com.aston.astonrecyclerview.data.Contact
 import com.aston.astonrecyclerview.databinding.MyRecyclerViewLayoutBinding
 
-class MyRecyclerViewAdapter(private val onClickAction: (Contact) -> Unit) : ListAdapter<Contact, MyRecyclerViewHolder>(MainDiffUtil) {
+class MyRecyclerViewAdapter(private val onClickAction: (Contact, position: Int, checkBox: CheckBox) -> Unit) : ListAdapter<Contact, MyRecyclerViewHolder>(MainDiffUtil) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyRecyclerViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.my_recycler_view_layout, parent, false)
         // val viewHolder = MyRecyclerViewLayoutBinding.inflate(inflater)
         // may be setOnclickListener add to viewHolder
         val viewHolder =  MyRecyclerViewHolder(view)
+
         viewHolder.itemView.setOnClickListener {
             val item = getItem(viewHolder.adapterPosition)
-            onClickAction(item)
+            val position = viewHolder.adapterPosition
+            onClickAction(item, position, viewHolder.checkBox)
+
         }
         return viewHolder
     }
@@ -25,6 +29,10 @@ class MyRecyclerViewAdapter(private val onClickAction: (Contact) -> Unit) : List
     override fun onBindViewHolder(holder: MyRecyclerViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
+
+
+
+
 
        // holder.itemView.setOnClickListener {
       //      if()
