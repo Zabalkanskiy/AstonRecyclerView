8014d69b1206bf4bab619bdeb9d89f0d65cdfc59
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-12-04 19:40:17 +0300

    add checkBox field

--- app/src/main/java/com/aston/astonrecyclerview/recycler/MyRecyclerViewHolder.kt
+++ app/src/main/java/com/aston/astonrecyclerview/recycler/MyRecyclerViewHolder.kt
@@ -1,6 +1,7 @@
 package com.aston.astonrecyclerview.recycler
 
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.aston.astonrecyclerview.R
@@ -12,6 +13,7 @@ class MyRecyclerViewHolder(itemView: View) : ViewHolder(itemView) {
     val name: TextView = itemView.findViewById(R.id.my_recycler_text_view_name)
     val surname: TextView = itemView.findViewById(R.id.my_recycler_text_view_surname)
     val phoneNumber: TextView = itemView.findViewById(R.id.my_recycler_text_view_phone_number)
+    val checkBox: CheckBox = itemView.findViewById(R.id.my_recycler_check_box)
     fun bind(model: Contact){
         name.text = model.name
         surname.text = model.surname
